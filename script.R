d7ef00de1934c90474eb77dd99c78aeb80daf707
Angela Gaetano <argaetano@mail.lipscomb.edu>
2018-05-03 18:55:11 -0500

    added spectral clustering for 3 and 4 k

--- script.R
+++ script.R
@@ -1,3 +1,5 @@
+pdf("SpectralClusteringPlots.pdf")
+
 dataSource <- 'http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data'
 dataFilePath <- './data/cancer.data'
 dataDirectoryPath <- './data'
@@ -24,3 +26,24 @@ plot(clusteringdata, col=specc1, main="breast cancer clusters")
 
 plot(cancer.data$V3,cancer.data$V4, col=c("red","black")[cancer.data$V2], main="Actual Clusters")
 legend("topright",pch=c(1,1), col=c("red", "black"),legend=c("Benign","Malignant"))
+
+
+specc2 <- specc(clusteringdata,3)
+
+par(mfrow=c(1,2))
+plot(clusteringdata, col=specc2, main="breast cancer clusters")
+
+plot(cancer.data$V3,cancer.data$V4, col=c("red","black")[cancer.data$V2], main="Actual Clusters")
+legend("topright",pch=c(1,1), col=c("red", "black"),legend=c("Benign","Malignant"))
+
+
+specc3 <- specc(clusteringdata,4)
+
+par(mfrow=c(1,2))
+plot(clusteringdata, col=specc3, main="breast cancer clusters")
+
+plot(cancer.data$V3,cancer.data$V4, col=c("red","black")[cancer.data$V2], main="Actual Clusters")
+legend("topright",pch=c(1,1), col=c("red", "black"),legend=c("Benign","Malignant"))
+
+
+graphics.off()

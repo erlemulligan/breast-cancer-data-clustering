7c45cf5956330a9e9af58dc12f40eb6e7095b1f2
Angela Gaetano <argaetano@mail.lipscomb.edu>
2018-05-06 17:01:56 -0500

    Added column headers to data

--- script.R
+++ script.R
@@ -1,19 +1,20 @@
 pdf("SpectralClusteringPlots.pdf")
 
-dataSource <- 'http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data'
-dataFilePath <- './data/cancer.data'
-dataDirectoryPath <- './data'
+# dataSource <- 'http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data'
+# dataFilePath <- './data/cancer.data'
+# dataDirectoryPath <- './data'
+# 
+# if (!dir.exists(dataDirectoryPath)) {
+#   dir.create(dataDirectoryPath)
+# }
+# 
+# if (!file.exists(dataFilePath)) {
+#   download.file(dataSource, destfile=dataFilePath)
+# }
+# 
+# cancer.data <- read.csv(dataFilePath, fileEncoding = "UTF-8", header = F)
 
-if (!dir.exists(dataDirectoryPath)) {
-  dir.create(dataDirectoryPath)
-}
-
-if (!file.exists(dataFilePath)) {
-  download.file(dataSource, destfile=dataFilePath)
-}
-
-cancer.data <- read.csv(dataFilePath, fileEncoding = "UTF-8", header = F)
-
+cancer.data <- read.csv("data.csv",header=TRUE)
 
 library(kernlab)
 
@@ -24,7 +25,7 @@ specc1 <- specc(clusteringdata,2)
 par(mfrow=c(1,2))
 plot(clusteringdata, col=specc1, main="breast cancer clusters")
 
-plot(cancer.data$V3,cancer.data$V4, col=c("red","black")[cancer.data$V2], main="Actual Clusters")
+plot(cancer.data$radius_mean,cancer.data$texture_mean, col=c("red","black")[cancer.data$diagnosis], main="Actual Clusters")
 legend("topright",pch=c(1,1), col=c("red", "black"),legend=c("Benign","Malignant"))
 
 
@@ -33,7 +34,7 @@ specc2 <- specc(clusteringdata,3)
 par(mfrow=c(1,2))
 plot(clusteringdata, col=specc2, main="breast cancer clusters")
 
-plot(cancer.data$V3,cancer.data$V4, col=c("red","black")[cancer.data$V2], main="Actual Clusters")
+plot(cancer.data$radius_mean,cancer.data$texture_mean, col=c("red","black")[cancer.data$diagnosis], main="Actual Clusters")
 legend("topright",pch=c(1,1), col=c("red", "black"),legend=c("Benign","Malignant"))
 
 
@@ -42,7 +43,7 @@ specc3 <- specc(clusteringdata,4)
 par(mfrow=c(1,2))
 plot(clusteringdata, col=specc3, main="breast cancer clusters")
 
-plot(cancer.data$V3,cancer.data$V4, col=c("red","black")[cancer.data$V2], main="Actual Clusters")
+plot(cancer.data$radius_mean,cancer.data$texture_mean, col=c("red","black")[cancer.data$diagnosis], main="Actual Clusters")
 legend("topright",pch=c(1,1), col=c("red", "black"),legend=c("Benign","Malignant"))
 
 
